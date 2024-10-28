9457efa22d11dd2a267a623246567ea5ec6053fa
omor faruk <md3147693@gmail.com>
2024-10-28 17:58:14 +0600

    dashboard and profile implemented successfully

--- assets/js/config.js
+++
@@ -1,1 +0,0 @@
-class Config{adjustLayout(){const i=document.getElementsByTagName("html")[0];window.innerWidth<=1200&&i.setAttribute("data-sidebar-size","full")}initWindowSize(){var t=this;window.addEventListener("resize",function(i){t.adjustLayout()})}init(){this.adjustLayout(),this.initWindowSize()}}(new Config).init();
